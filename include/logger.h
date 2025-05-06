413b0f0c68122799c31ff29f07ffdbd4135654d2
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-05-06 11:05:46 -0700

    Update logger to be consistent with other module loggers

--- include/logger.h
+++ include/logger.h
@@ -7,10 +7,8 @@ typedef enum {
     NONE = 0,
     DEBUG = 1,
     INFO = 2,
-    WARN = 3,     // CM: Delete when Kyle is ready
-//    WARNING = 3,  // CM: Change when Kyle is ready
-    ERROR = 4,    // CM: Delete when Kyle is ready
-//    SEVERE = 4,   // CM: Change when Kyle is ready
+    WARNING = 3,  // CM: Change when Kyle is ready
+    SEVERE = 4,   // CM: Change when Kyle is ready
     FATAL = 5,
 } LogLevel;
 
