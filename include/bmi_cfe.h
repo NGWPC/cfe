c2d4de516cef57846ac52c46c95a9378068b48b6
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-07-21 14:40:06 -0700

    Fix header file reference

--- include/bmi_cfe.h
+++ include/bmi_cfe.h
@@ -7,7 +7,6 @@ extern "C" {
 
 #include "./cfe.h"
 #include "bmi.h"
-#include "nash_cascade.h"
 
 //--------------------------------------------------
 // Experiment to simplify BMI implementation (SDP)
