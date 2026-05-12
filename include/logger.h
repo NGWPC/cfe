5d93227a19c090c4a0abca9045b7156af432893e
Carolyn.Maynard <carolyn.maynard@rtx.com>
2026-05-12 08:24:36 -0700

    Remove using generic EWTS_ID

--- include/logger.h
+++ include/logger.h
@@ -2,8 +2,12 @@
 #define CFE_LOGGER_H
 
 #include "ewts/module_constants.h"
-#define EWTS_ID EWTS_ID_CFE
 #include "ewts/logger.h"
 #include "ewts/log_levels.h"
+
+#define Log(level, ...) EwtsLogModule(EWTS_ID_CFE, (level), __VA_ARGS__)
+#define LOG(level, ...) EwtsLogModule(EWTS_ID_CFE, (level), __VA_ARGS__)
+#define GetLogLevel() EwtsGetLogLevelModule(EWTS_ID_CFE)
+#define IsLoggingEnabled() EwtsIsLoggingEnabledModule(EWTS_ID_CFE)
 
 #endif // CFE_LOGGER_H
